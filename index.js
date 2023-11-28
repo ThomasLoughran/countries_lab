ef1e44a987e8ec49b1c2a3b23482e01788b1bcb6
Thomas Loughran <tloughran3@gmail.com>
2023-11-28 15:56:43 +0000

    getCountryByName adds country name to index.html file

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 const getCountryByName = async (countryName) => {
+    // get country from api
     const url = 'https://restcountries.com/v3.1/name/' + countryName;
     const response = await fetch(url);
     const data = await response.json();
-    console.log(data[0]);
-    
+    const country = data[0];
 
+    // pass country information into a div and display it
+    const countryContainer = document.createElement('div');
+    countryContainer.innerText = country.name.common;
+    console.log(countryContainer);
+    document.querySelector('#foundCountry').appendChild(countryContainer);
 }
 
 getCountryByName('Brazil');
